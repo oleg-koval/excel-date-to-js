f003b292cdca6940260ecfbbee9eb3c87a74d0f2
Ivan Motiienko <Ivan_Motiienko@external.mckinsey.com>
2021-06-04 16:49:01 +0300

    feat: add ability to transform js date back to excel

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const SECONDS_IN_DAY = 24 * 60 * 60;
+const MISSING_LEAP_YEAR_DAY = SECONDS_IN_DAY * 1000;
+const MAGIC_NUMBER_OF_DAYS = (25567 + 2);
+
+const isDate = date => Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date.getTime())
+
 /**
  * JavaScript dates can be constructed by passing milliseconds
  * since the Unix epoch (January 1, 1970) example: new Date(12312512312);
@@ -14,18 +20,21 @@ module.exports.getJsDateFromExcel = (excelDate) => {
     throw new Error('wrong input format')
   }
 
-  const secondsInDay = 24 * 60 * 60;
-  const missingLeapYearDay = secondsInDay * 1000;
-  const delta = excelDate - (25567 + 2);
-  const parsed = delta * missingLeapYearDay;
+  const delta = excelDate - MAGIC_NUMBER_OF_DAYS;
+  const parsed = delta * MISSING_LEAP_YEAR_DAY;
   const date = new Date(parsed)
 
-  if (Object.prototype.toString.call(date) === "[object Date]" ) {
-    if (isNaN(date.getTime())) {
+  if (!isDate(date)) {
       throw new Error('wrong excel date input')
-    }
-    else {
-      return date
-    }
   }
+
+  return date
+}
+
+module.exports.getExcelDateFromJs = (date) => {
+  if (!isDate(date)) {
+      throw new Error('wrong input format')
+  }
+
+  return (date.getTime() / MISSING_LEAP_YEAR_DAY) + MAGIC_NUMBER_OF_DAYS
 }
