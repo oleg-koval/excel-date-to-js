280239efde8ea221626d528592d1889112cb4588
Francis Stokes <stokesie14@gmail.com>
2022-08-19 13:45:45 +0200

    Stricter compile time type check on input to match the runtime check in js

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,6 @@
 // https://github.com/oleg-koval/excel-date-to-js/issues/16
 
 declare module "excel-date-to-js" {
-  export function getJsDateFromExcel(excelDate: string | number): Date
+  export function getJsDateFromExcel(excelDate: number): Date
   export function getExcelDateFromJs(date: Date): number
 }
